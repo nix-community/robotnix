737a201cb12d6bf501e2699cb09de440e147166b
ajs124 <git@ajs124.de>
2019-03-22 23:40:21 +0100

    add nix-prefetch, this should work, I guess

--- hydra/prefetch.nix
+++ hydra/prefetch.nix
@@ -1,22 +1,20 @@
-{ nixdroid, ... }: with builtins; let
+{ nixdroid, pkgs ? import <unstable/nixpkgs> {}, ... }: with builtins; let
   h = import ./hydra.nix {};
+  nix-prefetch = pkgs.callPackage ../nix-prefetch.nix {};
 in {
-  prefetchers = derivation {
-    name = "nixdroid-prefetch.sh";
-    system = currentSystem;
-
-    builder = "/bin/sh";
-    args = let
-      mapAttrsToList = f: attrs: map (k: f k attrs.${k}) (attrNames attrs);
-      relevantAttrs = [ "repoRepoURL" "repoRepoRev" "referenceDir" "extraFlags" "localManifests" "device" "rev" "manifest" ]; # sha256
-      filterRelevantAttrs = s: listToAttrs (concatMap (k: if any (x: x == k) relevantAttrs then [{ name = k; value = s.${k}; }] else []) (attrNames s));
-      prefetchers = mapAttrsToList (x: y:
-        "nix-prefetch -f ${nixdroid}/repo2nix.nix --input json <<< '\"'\"'"  # Fuck escaping quotes
-          + toJSON (mapAttrs (k: v: v.value) (filterRelevantAttrs h.jobsets."${x}".inputs)) + "'\"'\"'") h.jobsets;
-    in [ (toFile "builder.sh" ''
-      echo $'#!/usr/bin/env nix-shell
-      #!nix-shell -i sh -p nix-prefetch
+  prefetchers = let
+    mapAttrsToList = f: attrs: map (k: f k attrs.${k}) (attrNames attrs);
+    relevantAttrs = [ "repoRepoURL" "repoRepoRev" "referenceDir" "extraFlags" "localManifests" "device" "rev" "manifest" ]; # sha256
+    filterRelevantAttrs = s: listToAttrs (concatMap (k: if any (x: x == k) relevantAttrs then [{ name = k; value = s.${k}; }] else []) (attrNames s));
+    prefetchers = mapAttrsToList (x: y:
+      "${nix-prefetch}/bin/nix-prefetch -f ${nixdroid}/repo2nix.nix --input json <<< '\"'\"'"  # Fuck escaping quotes
+        + toJSON (mapAttrs (k: v: v.value) (filterRelevantAttrs h.jobsets."${x}".inputs)) + "'\"'\"'") h.jobsets;
+  in
+    # FIXME drop the NIX_PATH override
+    pkgs.runCommand "nixdroid-prefetch.sh" {} ''
+      echo $'#!/bin/sh
+      export NIX_PATH=nixpkgs=/nix/var/nix/profiles/per-user/root/channels/unstable/
       ${concatStringsSep "\n" prefetchers}' > $out
-    '')];
-  };
+      chmod +x $out
+    '';
 }
