3a0658991191f3e18ba6a6ac847891213e5b9b65
ajs124 <git@ajs124.de>
2019-03-24 15:33:02 +0100

    reorganize roomservice XMLs, start to add support for roms other than LineageOS

--- hydra/hydra.nix
+++ hydra/hydra.nix
@@ -8,22 +8,28 @@ let
   };
   mkInput = t: v: e: { type = t; value = v; emailresponsible = e; };
   nixdroid = mkInput "git" "https://github.com/ajs124/NixDroid dev" true;
+  manifests = {
+    LineageOS = "https://github.com/LineageOS/android";
+    ResurrectionRemix = "https://github.com/ResurrectionRemix/platform_manifest";
+    UnlegacyAndroid = "https://github.com/Unlegacy-Android/android";
+    PixelExperience = "https://github.com/PixelExperience/manifest";
+  };
   defaultInputs = args: {
     inherit nixdroid;
     nixpkgs = mkInput "git" "https://github.com/nixos/nixpkgs-channels nixos-18.09" false;
     rev = mkInput "string" (optConf args "rev" "lineage-16.0") false;
     keyStorePath = mkInput "string" "/var/lib/nixdroid/keystore" false;
     device = mkInput "string" args.device false;
-    manifest = mkInput "string" (optConf args "manifest" "https://github.com/LineageOS/android.git") false;
+    manifest = mkInput "string" manifests."${args.rom}" false;
     sha256Path = mkInput "path" ("/var/lib/nixdroid/hashes/" + args.device + ".sha256") false;
     extraFlags = mkInput "string" (optConf args "extraFlags" "-g all,-darwin,-infra,-sts --no-repo-verify") false;
     opengappsVariant = mkInput "string" (optConf args "opengappsVariant" null) false;
     enableWireguard = mkInput "boolean" (optConf args "enableWireguard" "false") false;
     usePatchedCoreutils = mkInput "boolean" "true" false;
     localManifests = let
-      list = [ (../roomservice- + "${args.device}.xml") ] ++  # enableWireguard is a string, because hydra expects it to be one
-          (if (hasAttr "enableWireguard" args && args.enableWireguard == "true") then [ ../wireguard.xml ] else []) ++
-          (if (hasAttr "opengappsVariant" args) then [ ../opengapps.xml ] else []);
+      list = [ (../roomservice + "/${args.rom}/${args.device}.xml") ] ++  # enableWireguard is a string, because hydra expects it to be one
+          (if (hasAttr "enableWireguard" args && args.enableWireguard == "true") then [ ../roomservice/misc/wireguard.xml ] else []) ++
+          (if (hasAttr "opengappsVariant" args) then [ ../roomservice/misc/opengapps.xml ] else []);
       in {
       type = "nix";
       value = if isHydra then "[ " + toString list + " ]" else list;  # hydra does not take lists, only nix expressions
@@ -48,6 +54,7 @@ in {
       description = "LineageOS 15.1 for Hammerhead";
       inputs = defaultInputs {
         device = "hammerhead";
+        rom = "LineageOS";
         rev = "lineage-15.1";
         opengappsVariant = "pico";
       };
@@ -56,6 +63,7 @@ in {
       description = "LineageOS 15.1 for Payton";
       inputs = defaultInputs {
         device = "payton";
+        rom = "LineageOS";
         rev = "lineage-15.1";
         enableWireguard = "true";
         opengappsVariant = "nano";
@@ -65,6 +73,7 @@ in {
       description = "LineageOS 16.0 for OnePlus 3";
       inputs = defaultInputs {
         device = "oneplus3";
+        rom = "LineageOS";
         enableWireguard = "true";
         opengappsVariant = "nano";
       };
