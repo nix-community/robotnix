c39a4ea987e663d21613ada72ac6c0cb07156ffc
Daniel Fullmer <danielrf12@gmail.com>
2019-07-09 20:07:43 -0400

    F-Droid: Use prebuilt module for apk

--- modules/apps/fdroid.nix
+++ modules/apps/fdroid.nix
@@ -10,22 +10,6 @@ let
     rev = "0.2.9";
     sha256 = "0r2s7zyrkfhl88sal8jifhnq47s5p7bs340ifrm9pi7vq91ydvil";
   };
-  fdroidAndroidmk = pkgs.writeText "Android.mk" ''
-LOCAL_PATH:= $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := F-Droid
-LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := F-Droid
-
-LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := FDroid.apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-
-include $(BUILD_PREBUILT)
-  '';
 in
 {
   options = {
@@ -33,13 +17,11 @@ in
   };
 
   config = mkIf cfg.enable {
+    apps.prebuilt."F-Droid".apk = "${fdroid}/apk/full/release/app-full-release-unsigned.apk packages/apps/F-Droid/FDroid.apk";
+
     overlays."packages/apps/F-DroidPrivilegedExtension".contents = [ privext ];
 
     postPatch = ''
-      mkdir -p packages/apps/F-Droid
-      cp --no-preserve=all -v ${fdroidAndroidmk} packages/apps/F-Droid/Android.mk
-      cp --no-preserve=all -v ${fdroid}/apk/full/release/app-full-release-unsigned.apk packages/apps/F-Droid/FDroid.apk
-
       fdpe_hash() {
         ${pkgs.jdk}/bin/keytool -list -printcert -file "$1" | grep 'SHA256:' | tr --delete ':' | cut --delimiter ' ' --fields 3
       }
@@ -50,7 +32,7 @@ in
        --replace 43238d512c1e5eb2d6569f4a3afbf5523418b82e0a3ed1552770abb9a9c9ccab "$platform_hash"
     '';
 
-    additionalProductPackages = [ "F-Droid" "F-DroidPrivilegedExtension" ];
+    additionalProductPackages = [ "F-DroidPrivilegedExtension" ];
 
     patches = [ ./fdroid/privext.patch ];
   };
