ba60405751b7b002b288d426ca37f807c4641eb1
Daniel Fullmer <danielrf12@gmail.com>
2019-07-13 16:28:13 -0400

    bromite: Update to latest systemwebview

--- example.nix
+++ example.nix
@@ -2,7 +2,7 @@ with (import <nixpkgs> {});
 {
   imports = [ ./modules/profiles/grapheneos.nix ];
   device = "marlin";
-  buildID = "2019.07.04.1";
+  buildID = "2019.07.13.1"; # Don't forget to update for each unique build
   certs = {
     verity = ./keys/verity.x509.pem;  # Only necessary for marlin (Pixel XL) since the kernel build needs to include this cert
     platform = ./keys/platform.x509.pem;  # Used by fdroid privileged extension to whitelist org.fdroid.fdroid
@@ -13,8 +13,8 @@ with (import <nixpkgs> {});
       description = "Bromite";
       packageName = "com.android.webview";
       apk = fetchurl {
-        url = "https://github.com/bromite/bromite/releases/download/75.0.3770.109/arm64_SystemWebView.apk";
-        sha256 = "1jlhf3np7a9zy0gjsgkhykik4cfs5ldmhgb4cfqnpv4niyqa9xxx";
+        url = "https://github.com/bromite/bromite/releases/download/75.0.3770.139/arm64_SystemWebView.apk";
+        sha256 = "0kxlvc3asvi4dhqkps0nhmfljk5mq5lc6vihj2acc3z7r7gy9yx4";
       };
     };
 
