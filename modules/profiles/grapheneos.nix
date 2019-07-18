7bc9833c3294e52f550dcf8beeba93a84b93250b
Daniel Fullmer <danielrf12@gmail.com>
2019-07-17 22:00:38 -0400

    grapheneos: Update to 2019.07.16.22

--- modules/profiles/grapheneos.nix
+++ modules/profiles/grapheneos.nix
@@ -2,16 +2,16 @@
 with lib;
 let
   tag = {
-    marlin = "PQ3A.190705.001.2019.07.01.21";
-    taimen = "PQ3A.190705.001.2019.07.01.21";
-    crosshatch = "PQ3A.190705.003.2019.07.01.21";
-    bonito = "PQ3B.190705.003.2019.07.01.21";
+    marlin = "PQ3A.190705.001.2019.07.16.22";
+    taimen = "PQ3A.190705.001.2019.07.16.22";
+    crosshatch = "PQ3A.190705.003.2019.07.16.22";
+    bonito = "PQ3B.190705.003.2019.07.16.22";
   }.${config.deviceFamily};
   releases = {
     # TODO: Add all releases
-    "PQ3A.190705.001.2019.07.01.21" = {
-      rev = "094a8e7d070094a6b927a615de3254ce0640ddb5";
-      sha256 = "0k24946sxad8pn560sjianpf0d5mpn38dv0yw3pm6x3hj64j8g58";
+    "PQ3A.190705.001.2019.07.16.22" = {
+      rev = "1bd882ec78e3740ef981004919cbfe6386e218c3";
+      sha256 = "1jy47x1qg87yv190jssqpdmx4w1632g0jnnhiszcmdqkkhwr9pwd";
     };
   };
 in
@@ -26,6 +26,7 @@ in
 
   apps.webview.enable = mkDefault true;
   # TODO: Build and include vanadium
+  removedProductPackages = [ "Vanadium" ];
 
   apps.updater.enable = mkDefault true;
   apps.updater.src = mkDefault null; # Already included in platform manifest
