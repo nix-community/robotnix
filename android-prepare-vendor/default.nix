a8a9a3d7914c7d62adc67b1182704bfd5f746060
Daniel Fullmer <danielrf12@gmail.com>
2019-09-06 15:45:19 -0400

    Add android-prepare-vendor unpackImg

--- android-prepare-vendor/default.nix
+++ android-prepare-vendor/default.nix
@@ -8,4 +8,11 @@ rec {
     runCommand "vendor-files-${device}" {} ''
       ${android-prepare-vendor}/execute-all.sh ${lib.optionalString full "--full"} --yes --output $out --device "${device}" --buildID "${buildID}" -i "${img}" --debugfs --timestamp "${builtins.toString timestamp}"
     '';
+
+  unpackImg =
+    { device, img }:
+    runCommand "unpacked-img-${device}" {} ''
+      mkdir -p $out
+      ${android-prepare-vendor}/scripts/extract-factory-images.sh --debugfs --input "${img}" --output $out --conf-file ${android-prepare-vendor}/${device}/config.json
+    '';
 }
