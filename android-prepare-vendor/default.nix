0bb9bccea948d4d69d1ac4bd8fe9c5178ae2467d
Daniel Fullmer <danielrf12@gmail.com>
2019-09-13 19:30:56 -0400

    vendor: Allow for custom android-prepare-vendor config

--- android-prepare-vendor/default.nix
+++ android-prepare-vendor/default.nix
@@ -4,15 +4,15 @@ rec {
   android-prepare-vendor = callPackage ./android-prepare-vendor.nix { inherit api; };
 
   buildVendorFiles =
-    { device, img, full ? false, timestamp ? 1, buildID ? "nixdroid" }:
+    { device, img, full ? false, timestamp ? 1, buildID ? "nixdroid", configFile ? null }:
     runCommand "vendor-files-${device}" {} ''
-      ${android-prepare-vendor}/execute-all.sh ${lib.optionalString full "--full"} --yes --output . --device "${device}" --buildID "${buildID}" -i "${img}" --debugfs --timestamp "${builtins.toString timestamp}"
+      ${android-prepare-vendor}/execute-all.sh ${lib.optionalString full "--full"} --yes --output . --device "${device}" --buildID "${buildID}" -i "${img}" --debugfs --timestamp "${builtins.toString timestamp}" ${lib.optionalString (configFile != null) "--conf-file ${configFile}"}
       mkdir -p $out
       cp -r ${device}/${buildID}/* $out
     '';
 
   unpackImg =
-    { device, img }:
+    { device, img, configFile ? null }:
     runCommand "unpacked-img-${device}" {} ''
       mkdir -p $out
       ${android-prepare-vendor}/scripts/extract-factory-images.sh --debugfs --input "${img}" --output $out --conf-file ${android-prepare-vendor}/${device}/config.json
--- modules/vendor.nix
+++ modules/vendor.nix
@@ -1,7 +1,10 @@
 { config, pkgs, lib, ... }:
 
-
+# TODO: One possibility is to reimplement parts of android-prepare-vendor in native nix so we can use an android-prepare-vendor config file directly
 with lib;
+let
+  android-prepare-vendor = pkgs.callPackage ../android-prepare-vendor { api = config.apiLevel; };
+in
 {
   options = {
     vendor.img = mkOption {
@@ -13,19 +16,49 @@ with lib;
     vendor.full = mkOption {
       default = false;
       type = types.bool;
-      description = "Include non-essential OEM blobs to be compatible with GApps";
+      description = "Include non-essential OEM blobs";
     };
 
     vendor.files = mkOption {
       type = types.path;
       internal = true;
     };
+
+    vendor.systemBytecode = mkOption {
+      type = types.listOf types.str;
+      default = [];
+    };
+
+    vendor.systemOther = mkOption {
+      type = types.listOf types.str;
+      default = [];
+    };
   };
 
   config = mkIf (config.vendor.img != null) {
-    vendor.files = (pkgs.callPackage ../android-prepare-vendor { api = config.apiLevel; }).buildVendorFiles {
+    vendor.files = let
+      apvConfig = builtins.fromJSON (builtins.readFile "${android-prepare-vendor.android-prepare-vendor}/${config.device}/config.json");
+      # TODO: There's probably a better way to do this
+      mergedConfig = recursiveUpdate apvConfig {
+        "api-${config.apiLevel}".${if config.vendor.full then "full" else "naked"} = let
+          _config = apvConfig."api-${config.apiLevel}".${if config.vendor.full then "full" else "naked"};
+        in _config // {
+          system-bytecode = _config.system-bytecode ++ config.vendor.systemBytecode;
+          system-other = _config.system-other ++ config.vendor.systemOther;
+        };
+      };
+      mergedConfigFile = builtins.toFile "config.json" (builtins.toJSON mergedConfig);
+    in
+      android-prepare-vendor.buildVendorFiles {
+        inherit (config) device;
+        inherit (config.vendor) img full;
+        configFile = mergedConfigFile;
+      };
+
+    # Just for ease in debugging
+    build.vendorUnpacked = android-prepare-vendor.unpackImg {
       inherit (config) device;
-      inherit (config.vendor) img full;
+      inherit (config.vendor) img;
     };
 
     # Using unpackScript instead of source.dirs since vendor_overlay/google_devices/${config.device} is not guaranteed to exist
