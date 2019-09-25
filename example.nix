118d1783843dbf4aa8da5ec3d4f036925d4567c7
Daniel Fullmer <danielrf12@gmail.com>
2019-09-24 23:27:40 -0400

    example.nix: Increment buildNumber

--- example.nix
+++ example.nix
@@ -6,8 +6,8 @@ with lib;
   flavor = "grapheneos"; # "vanilla" is another option
 
   # Don't forget to update these for each unique build. Needed for the updater
-  buildNumber = "2019.09.16.1";
-  buildDateTime = 1568649711;
+  buildNumber = "2019.09.24.1";
+  buildDateTime = 1569381701;
 
   # A _string_ of the path for the key store.
   keyStorePath = "/var/secrets/android-keys";
