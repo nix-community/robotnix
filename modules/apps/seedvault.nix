1dc0868776ac5e8eb3fa46ebd253ac80e5bb9120
Daniel Fullmer <danielrf12@gmail.com>
2020-03-26 21:04:40 -0400

    seedvault: make it finally work

--- modules/apps/seedvault.nix
+++ modules/apps/seedvault.nix
@@ -13,22 +13,24 @@ in
     apps.prebuilt.Seedvault = {
       apk = apks.seedvault;
       packageName = "com.stevesoltys.seedvault";
+      certificate = "platform"; # Needs this certificate to use MANAGE_DOCUMENTS permission
       privileged = true;
-      privappPermissions = [ "BACKUP" "MANAGE_USB" "MANAGE_DOCUMENTS" "WRITE_SECURE_SETTINGS" "INSTALL_PACKAGES" ];
+      privappPermissions = [ "BACKUP" "MANAGE_USB" "WRITE_SECURE_SETTINGS" "INSTALL_PACKAGES" ];
     };
 
     etc."sysconfig/com.stevesoltys.seedvault.xml".text = ''
       <?xml version="1.0" encoding="utf-8"?>
       <config>
         <backup-transport-whitelisted-service
-          service="com.stevesoltys.seedvault.transport.ConfigurableBackupTransportService"/>
+          service="com.stevesoltys.seedvault/.transport.ConfigurableBackupTransportService"/>
       </config>
     '';
 
     # Set as default
-    resources."frameworks/base/packages/SettingsProvider".def_backup_transport = "com.stevesoltys.seedvault.transport.ConfigurableBackupTransportService";
+    resources."frameworks/base/packages/SettingsProvider".def_backup_enabled = true;
+    resources."frameworks/base/packages/SettingsProvider".def_backup_transport = "com.stevesoltys.seedvault.transport.ConfigurableBackupTransport";
 
-    # TODO: is the above working?
+    # In order to switch to using this if it's not set on the first boot, run these:
     # $ bmgr list transports
     # $ bmgr transport com.stevesoltys.seedvault.transport.ConfigurableBackupTransport
   };
