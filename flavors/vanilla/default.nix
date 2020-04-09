7ac16c133ccebb938d008d85f8b4c6635d9f7f17
Daniel Fullmer <danielrf12@gmail.com>
2020-04-09 12:27:48 -0400

    vanilla: set AOSP usability improvements unconditionally

--- flavors/vanilla/default.nix
+++ flavors/vanilla/default.nix
@@ -7,12 +7,45 @@ let
     url = "https://android.googlesource.com/kernel/msm";
     inherit rev sha256;
   };
-  supportedDeviceFamilies = [ "marlin" "taimen" "muskie" "crosshatch" "bonito" "coral" ];
 in mkIf (config.flavor == "vanilla") (mkMerge [
 {
   source.jsonFile = ./. + "/${config.source.manifest.rev}.json";
   # Not strictly necessary for me to set this, since I override the jsonFile
   source.manifest.url = mkDefault "https://android.googlesource.com/platform/manifest";
+
+  ### AOSP usability improvements ###
+
+  # This is the prebuilt webview apk from AOSP. It is very old and not enabled by default.
+  # Enable using webview.prebuild.enable = true;
+  webview.prebuilt.apk = config.source.dirs."external/chromium-webview".contents + "/prebuilt/${config.arch}/webview.apk";
+  webview.prebuilt.availableByDefault = mkDefault true;
+
+  # Instead, we build our own chromium and webview
+  apps.chromium.enable = mkDefault true;
+  webview.chromium.availableByDefault = mkDefault true;
+  webview.chromium.enable = mkDefault true;
+
+  removedProductPackages = [ "webview" "Browser2" "QuickSearchBox" ];
+  source.dirs."external/chromium-webview".enable = false;
+  source.dirs."packages/apps/QuickSearchBox".enable = false;
+  source.dirs."packages/apps/Browser2".enable = false;
+
+  source.dirs."packages/apps/Launcher3".patches = [ (./. + "/${toString config.androidVersion}/disable-quicksearch.patch") ];
+  source.dirs."device/google/marlin".patches = [ (./. + "/${toString config.androidVersion}/marlin-fix-device-names.patch") ];
+  source.dirs."device/google/taimen".patches = [ (./. + "/${toString config.androidVersion}/taimen-fix-device-names.patch") ];
+  source.dirs."device/google/muskie".patches = [ (./. + "/${toString config.androidVersion}/muskie-fix-device-names.patch") ];
+  source.dirs."device/google/crosshatch".patches = [ (./. + "/${toString config.androidVersion}/crosshatch-fix-device-names.patch") ];
+  source.dirs."device/google/bonito".patches = [ (./. + "/${toString config.androidVersion}/bonito-fix-device-names.patch") ];
+
+  source.dirs."packages/apps/DeskClock".patches = mkIf (config.androidVersion == 10) [
+    (pkgs.fetchpatch {
+      url = "https://github.com/GrapheneOS/platform_packages_apps_DeskClock/commit/f31333513b1bf27ae23c61e4ba938568cc9e7b76.patch";
+      sha256 = "1as8vyhfyi9cj61fc80ajskyz4lwwdc85fgxhj0b69z0dbxm77pj";
+    })
+  ];
+
+  resources."frameworks/base/core/res".config_swipe_up_gesture_setting_available = true; # enable swipe up gesture functionality as option
+  resources."packages/apps/Settings".config_use_legacy_suggestion = false; # fix for cards not disappearing in settings app
 }
 (mkIf ((elem config.deviceFamily [ "taimen" "muskie" "bonito" "crosshatch" "coral" ]) || (config.device == "x86")) {
   vendor.buildID = mkDefault "QQ2A.200405.005";
@@ -64,37 +97,5 @@ in mkIf (config.flavor == "vanilla") (mkMerge [
   # could normally be fetched with repo at https://android.googlesource.com/kernel/manifest
   # but google didn't push a branch like android-msm-crosshatch-4.9-pie-qpr3 to that repo.
   kernel.useCustom = mkDefault config.signBuild;
-})
-
-# AOSP usability improvements for device builds.
-(mkIf (elem config.deviceFamily supportedDeviceFamilies) {
-  # This is the prebuilt webview apk from AOSP. It is very old and not enabled by default
-  webview.prebuilt.apk = config.source.dirs."external/chromium-webview".contents + "/prebuilt/${config.arch}/webview.apk";
-  webview.prebuilt.availableByDefault = mkDefault true;
-
-  # Instead, we build our own chromium and webview
-  apps.chromium.enable = mkDefault true;
-  webview.chromium.availableByDefault = mkDefault true;
-  webview.chromium.enable = mkDefault true;
-
-  removedProductPackages = [ "webview" "Browser2" "QuickSearchBox" ];
-  source.dirs."external/chromium-webview".enable = false;
-  source.dirs."packages/apps/QuickSearchBox".enable = false;
-  source.dirs."packages/apps/Browser2".enable = false;
-
-  source.dirs."packages/apps/Launcher3".patches = [ (./. + "/${toString config.androidVersion}" + /disable-quicksearch.patch) ];
-  source.dirs."device/google/${config.deviceFamily}".patches = [
-    (./. + "/${toString config.androidVersion}/${config.deviceFamily}-fix-device-names.patch")
-  ];
-
-  source.dirs."packages/apps/DeskClock".patches = mkIf (config.androidVersion == 10) [
-    (pkgs.fetchpatch {
-      url = "https://github.com/GrapheneOS/platform_packages_apps_DeskClock/commit/f31333513b1bf27ae23c61e4ba938568cc9e7b76.patch";
-      sha256 = "1as8vyhfyi9cj61fc80ajskyz4lwwdc85fgxhj0b69z0dbxm77pj";
-    })
-  ];
-
-  resources."frameworks/base/core/res".config_swipe_up_gesture_setting_available = true; # enable swipe up gesture functionality as option
-  resources."packages/apps/Settings".config_use_legacy_suggestion = false; # fix for cards not disappearing in settings app
 })
 ])
