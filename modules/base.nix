af75ed08080ab2f09f1f378bf3aff5f7539949d0
Daniel Fullmer <danielrf12@gmail.com>
2020-05-08 17:47:33 -0400

    Add envPackages config option

--- modules/base.nix
+++ modules/base.nix
@@ -138,6 +138,12 @@ in
 
     ccache.enable = mkEnableOption "ccache";
 
+    envPackages = mkOption {
+      type = types.listOf types.package;
+      internal = true;
+      default = [];
+    };
+
     # Random attrset to throw build products into
     build = mkOption {
       internal = true;
@@ -420,51 +426,7 @@ in
 
       env = pkgs.buildFHSUserEnv {
         name = "robotnix-build";
-        # Check build/soong/ui/build/paths/config.go for a list of things that are needed
-        targetPkgs = pkgs: with pkgs; [
-          bc
-          git
-          gnumake
-          jre8_headless
-          lsof
-          m4
-          ncurses5
-          openssl # Used in avbtool
-          psmisc # for "fuser", "pstree"
-          rsync
-          unzip
-          zip
-
-          # Things not in build/soong/ui/build/paths/config.go
-          nettools # Needed for "hostname" in build/soong/ui/build/sandbox_linux.go
-          procps # Needed for "ps" in build/envsetup.sh
-        ] ++ optionals (config.androidVersion <= 9) [
-          # stuff that was in the earlier buildenv. Not entirely sure everything here is necessary
-          (androidPkgs.sdk (p: with p.stable; [ tools platform-tools ]))
-          openssl.dev
-          bison
-          curl
-          flex
-          gcc
-          gitRepo
-          gnupg
-          gperf
-          imagemagick
-          libxml2
-          lzip
-          lzop
-          perl
-          python2
-          schedtool
-          utillinux
-          which
-        ] ++ optionals (config.androidVersion >= 10) [
-          freetype # Needed by jdk9 prebuilt
-          fontconfig
-
-          python3
-          python2 # device/generic/goldfish/tools/mk_combined_img.py still needs py2 :(
-        ];
+        targetPkgs = pkgs: config.envPackages;
         multiPkgs = pkgs: with pkgs; [ zlib ];
       };
     };
