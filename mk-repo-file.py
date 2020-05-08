a087e4c658366b1eba5b233f59d595074bcdb541
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-05-08 17:56:29 -0400

    mk-repo-file: Configurable ref type

--- mk-repo-file.py
+++ mk-repo-file.py
@@ -2,6 +2,7 @@
 #!nix-shell -i python -p python3 nix gitRepo nix-prefetch-git -I nixpkgs=./pkgs
 
 from typing import Optional, Dict
+from enum import Enum
 
 import argparse
 import json
@@ -23,6 +24,12 @@ revHashes: Dict[str, str] = {}
 revTrees: Dict[str, str] = {}
 treeHashes: Dict[str, str] = {}
 
+# The kind of remote a "commitish" refers to.
+# These are used for the --ref-type CLI arg.
+class ManifestRefType(Enum):
+    BRANCH = "heads"
+    TAG = "tags"
+
 def save(filename, data):
     open(filename, 'w').write(json.dumps(data, sort_keys=True, indent=2, separators=(',', ': ')))
 
@@ -31,13 +38,13 @@ def checkout_git(url, rev):
     json_text = subprocess.check_output([ "nix-prefetch-git", "--url", url, "--rev", rev]).decode()
     return json.loads(json_text)
 
-def make_repo_file(url: str, rev: str, filename: str, mirror: Optional[str]=None):
+def make_repo_file(url: str, rev: str, filename: str, ref_type: ManifestRefType, mirror: Optional[str]=None):
     if os.path.exists(filename):
         data = json.load(open(filename))
     else:
         print("Fetching information for %s %s" % (url, rev))
         with tempfile.TemporaryDirectory() as tmpdir:
-            subprocess.check_call(['repo', 'init', '--manifest-url=' + url, '--manifest-branch=refs/heads/' + rev, *REPO_FLAGS], cwd=tmpdir)
+            subprocess.check_call(['repo', 'init', '--manifest-url=' + url, '--manifest-branch=refs/' + ref_type.value + '/' + rev, *REPO_FLAGS], cwd=tmpdir)
             json_text = subprocess.check_output(['repo', 'dumpjson'], cwd=tmpdir).decode()
             open(filename, 'w').write(json_text)
             data = json.loads(json_text)
@@ -79,10 +86,13 @@ def make_repo_file(url: str, rev: str, filename: str, mirror: Optional[str]=None
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--mirror', help="directory to a repo mirror of %s" % AOSP_BASEURL)
+    parser.add_argument('--ref-type', help="the kind of ref that is mirrored", choices=[t.name.lower() for t in ManifestRefType], default=ManifestRefType.TAG.name.lower())
     parser.add_argument('url', help="manifest URL")
     parser.add_argument('rev', help="manifest revision/tag")
     parser.add_argument('oldrepojson', nargs='*', help="any older repo json files to use for cached sha256s")
     args = parser.parse_args()
+
+    ref_type = ManifestRefType[args.ref_type.upper()]
 
     # Read all oldrepojson files to populate hashtables
     for filename in args.oldrepojson:
@@ -94,7 +104,7 @@ def main():
                     treeHashes[p['tree']] = p['sha256']
                     revTrees[p['rev']] = p['tree']
 
-    make_repo_file(args.url, args.rev, args.rev + '.json', mirror=args.mirror)
+    make_repo_file(args.url, args.rev, args.rev + '.json', ref_type=ref_type, mirror=args.mirror)
 
 if __name__ == "__main__":
     main()
