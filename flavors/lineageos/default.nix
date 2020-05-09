0061d3ae165a979381d2d3792c1e82e9daf767c1
Daniel Fullmer <danielrf12@gmail.com>
2020-05-09 00:19:58 -0400

    lineageos: refactor source.dirs using mkMerge

--- flavors/lineageos/default.nix
+++ flavors/lineageos/default.nix
@@ -11,7 +11,7 @@ let
 
   date = "2020.05.04";
   LineageOSRelease = "lineage-17.1";
-in mkIf (config.flavor == "lineageos") (mkMerge [
+in mkIf (config.flavor == "lineageos")
 {
   productNamePrefix = "lineage"; # product names start with "lineage_"
 
@@ -20,37 +20,35 @@ in mkIf (config.flavor == "lineageos") (mkMerge [
   buildDateTime = mkDefault 1588648528;
   #vendor.buildID = mkDefault "lineage-17.0-${date}";
 
-  source.dirs = lib.importJSON (./. + "/repo-${LineageOSRelease}.json");
+  source.dirs = mkMerge [
+    (lib.importJSON (./. + "/repo-${LineageOSRelease}.json"))
+
+    # Device-specific source dirs
+    (let
+      relpaths = map (d: replaceStrings ["_"] ["/"] (removePrefix "android_" d)) deviceMetadata.${config.device}.deps;
+     in filterDirsAttrs (getAttrs relpaths deviceDirs))
+
+    # Vendor-specific source dirs
+    (let
+      oem = toLower deviceMetadata.${config.device}.oem;
+      relpath = "vendor/${if oem == "lg" then "lge" else oem}";
+     in filterDirsAttrs (getAttrs [relpath] vendorDirs))
+
+    {
+      "vendor/lineage".patches = [ ./0001-Remove-LineageOS-keys.patch ];
+      "system/extras".patches = [
+        # pkgutil.get_data() not working, probably because we don't use their compiled python
+        (pkgs.fetchpatch {
+          url = "https://github.com/LineageOS/android_system_extras/commit/7da4b29321eb7ebce9eb9a43d0fbd85d0aa1e870.patch";
+          sha256 = "0pv56lypdpsn66s7ffcps5ykyfx0hjkazml89flj7p1px12zjhy1";
+          revert = true;
+        })
+      ];
+    }
+  ];
 
   source.manifest.url = mkDefault "https://github.com/LineageOS/android.git";
   source.manifest.rev = mkDefault "refs/heads/${LineageOSRelease}";
 
   envPackages = [ pkgs.openssl.dev ]; # Needed by included kernel build for some devices (pioneer at least)
 }
-{
-  # Device-specific source dirs
-  source.dirs =
-    let
-      relpaths = map (d: replaceStrings ["_"] ["/"] (removePrefix "android_" d)) deviceMetadata.${config.device}.deps;
-    in filterDirsAttrs (getAttrs relpaths deviceDirs);
-}
-{
-  # Vendor-specific source dirs
-  source.dirs =
-    let
-      oem = toLower deviceMetadata.${config.device}.oem;
-      relpath = "vendor/${if oem == "lg" then "lge" else oem}";
-    in filterDirsAttrs (getAttrs [relpath] vendorDirs);
-}
-{
-  source.dirs."vendor/lineage".patches = [ ./0001-Remove-LineageOS-keys.patch ];
-  source.dirs."system/extras".patches = [
-    # pkgutil.get_data() not working, probably because we don't use their compiled python
-    (pkgs.fetchpatch {
-      url = "https://github.com/LineageOS/android_system_extras/commit/7da4b29321eb7ebce9eb9a43d0fbd85d0aa1e870.patch";
-      sha256 = "0pv56lypdpsn66s7ffcps5ykyfx0hjkazml89flj7p1px12zjhy1";
-      revert = true;
-    })
-  ];
-}
-])
