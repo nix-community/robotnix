85a08dfe1bc220cbecd3b1e78697855871ae70c7
Daniel Fullmer <danielrf12@gmail.com>
2020-05-17 01:18:44 -0400

    Reproducibility fix for avb salt not being set

--- modules/10/default.nix
+++ modules/10/default.nix
@@ -7,18 +7,39 @@ let
   hash = builtins.hashString "sha256" "${config.buildNumber} ${builtins.toString config.buildDateTime}";
 in
 mkIf (config.androidVersion >= 10) {
-  source.dirs."build/make".patches = [
-    ./build_make/0001-Readonly-source-fix.patch
-    (pkgs.substituteAll {
-      src = ./build_make/0002-Partition-size-fix.patch;
-      inherit (pkgs) coreutils;
-    })
-    ./build_make/0003-Make-vendor_manifest.xml-reproducible.patch
-    (pkgs.substituteAll {
-      src = ./build_make/0004-Set-uuid-and-hash_seed-for-userdata-and-cache.patch;
-      inherit hash;
-    })
-  ];
+  source.dirs."build/make" = {
+    patches = [
+      ./build_make/0001-Readonly-source-fix.patch
+      (pkgs.substituteAll {
+        src = ./build_make/0002-Partition-size-fix.patch;
+        inherit (pkgs) coreutils;
+      })
+      ./build_make/0003-Make-vendor_manifest.xml-reproducible.patch
+      (pkgs.substituteAll {
+        src = ./build_make/0004-Set-uuid-and-hash_seed-for-userdata-and-cache.patch;
+        inherit hash;
+      })
+      ./build_make/0005-Add-marker-to-insert-AVB-salt-flags.patch
+    ];
+
+    postPatch = let
+      # Make salt based on combination of partition name and hash. Needed for reproducibility.
+      salt = partition: builtins.hashString "sha256" "${partition} ${hash}";
+      avbSaltFlags = pkgs.writeText "avb_salt_flags" ''
+        BOARD_AVB_BOOT_ADD_HASH_FOOTER_ARGS += --salt ${salt "boot"}
+        BOARD_AVB_DTBO_ADD_HASH_FOOTER_ARGS += --salt ${salt "dtbo"}
+        BOARD_AVB_SYSTEM_ADD_HASHTREE_FOOTER_ARGS += --salt ${salt "system"}
+        BOARD_AVB_SYSTEM_OTHER_ADD_HASHTREE_FOOTER_ARGS += --salt ${salt "system_other"}
+        BOARD_AVB_VENDOR_ADD_HASHTREE_FOOTER_ARGS += --salt ${salt "vendor"}
+        BOARD_AVB_RECOVERY_ADD_HASH_FOOTER_ARGS += --salt ${salt "recovery"}
+        BOARD_AVB_PRODUCT_ADD_HASHTREE_FOOTER_ARGS += --salt ${salt "product"}
+        BOARD_AVB_PRODUCT_SERVICES_ADD_HASHTREE_FOOTER_ARGS += --salt ${salt "product_services"}
+        BOARD_AVB_ODM_ADD_HASHTREE_FOOTER_ARGS += --salt ${salt "odm"}
+      '';
+    in ''
+      sed -i -e '/### AVB SALT MARKER ###/r ${avbSaltFlags}' core/Makefile
+    '';
+  };
 
   # This one script needs python2. Used by sdk builds
   source.dirs."development".postPatch = ''
