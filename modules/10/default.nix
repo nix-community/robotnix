02e21f32aa70795f9dc0866e7c7ac77f10bb5d24
Daniel Fullmer <danielrf12@gmail.com>
2020-05-17 00:55:49 -0400

    Reorganize build/make patches for android 10

--- modules/10/default.nix
+++ modules/10/default.nix
@@ -1,15 +1,23 @@
 { config, pkgs, lib, ... }:
 
 with lib;
+let
+  # Some mostly-unique data used as input for filesystem UUIDs, hash_seeds, and AVB salt.
+  # TODO: Maybe include all source hashes except from build/make to avoid infinite recursion?
+  hash = builtins.hashString "sha256" "${config.buildNumber} ${builtins.toString config.buildDateTime}";
+in
 mkIf (config.androidVersion >= 10) {
   source.dirs."build/make".patches = [
-    ./build-make-readonly-source.patch
-    (pkgs.substituteAll { # Alternative fix was upstreamed in https://android-review.googlesource.com/c/platform/build/+/1269603
-      src = ./build-make-partition-size-fix.patch;
+    ./build_make/0001-Readonly-source-fix.patch
+    (pkgs.substituteAll {
+      src = ./build_make/0002-Partition-size-fix.patch;
       inherit (pkgs) coreutils;
     })
-    ./build-make-vendor_manifest-reproducible.patch
-    ./build-make-userdata-cache-uuid-reproducible.patch
+    ./build_make/0003-Make-vendor_manifest.xml-reproducible.patch
+    (pkgs.substituteAll {
+      src = ./build_make/0004-Set-uuid-and-hash_seed-for-userdata-and-cache.patch;
+      inherit hash;
+    })
   ];
 
   # This one script needs python2. Used by sdk builds
