2c506576382b7dc0343beac6117eca1e6ea23c59
Daniel Fullmer <danielrf12@gmail.com>
2020-05-21 16:28:46 -0400

    android-prepare-vendor: 2019-07-13 -> 2020-05-21
    Remove support for API 28

--- pkgs/android-prepare-vendor/default.nix
+++ pkgs/android-prepare-vendor/default.nix
@@ -1,10 +1,9 @@
 { stdenv, lib, callPackage, fetchurl, fetchpatch, fetchFromGitHub, autoPatchelfHook, makeWrapper,
-  simg2img, zip, unzip, e2fsprogs, jq, jdk, wget, utillinux, perl, which, python2,
-  api ? "28"
+  simg2img, zip, unzip, e2fsprogs, jq, jdk, curl, utillinux, perl, python2,
+  api ? "29"
 }:
 
 let
-  buildID = "robotnix"; # Doesn't have to match the real buildID
   dexrepair = callPackage ./dexrepair.nix {};
 
   # TODO: This is for API-28. Need to make this work for all of them. Preferably without downloading each one
@@ -32,24 +31,17 @@ let
 in
 (stdenv.mkDerivation {
   pname = "android-prepare-vendor";
-  version = "2019-07-13";
+  version = "2020-05-21";
 
-  # TODO: Unify these
-  src = if (api == "28") then (fetchFromGitHub {
-    owner = "anestisb";
-    repo = "android-prepare-vendor";
-    rev = "e853d17c89f6962d3fd6f408db8576e6b445f643";
-    sha256 = "1aicx4lh1gvrbq4llh0dqifhp3y5d4g44r271b2qbg3vpkz48alb";
-  }) else
-  (fetchFromGitHub { # api == "29"
+  src = fetchFromGitHub { # api == "29"
     owner = "danielfullmer";
     repo = "android-prepare-vendor";
-    rev = "059c4b69765c296e3efc8938bb49e4f129204c54";
-    sha256 = "1hcq6d9fxw5dj5473r8xs6wppd7py9lffrvqpzk661inw6ndp696";
-  });
+    rev = "84f0d7dee8ab25eea7e8c023369112b5e10f657b";
+    sha256 = "1bhxy76gibax8sxfcs1bxc116261wf22p5r8apvq7l7qzrib1g02";
+  };
 
   nativeBuildInputs = [ makeWrapper ];
-  buildInputs = lib.optionals (api == "29") [
+  buildInputs = [
     (python2.withPackages (p: [ p.protobuf ])) # Python is used by "extract_android_ota_payload"
   ];
 
@@ -65,7 +57,7 @@ in
     cp -r ${oatdump}/* hostTools/Linux/api-${api}/
 
     for i in ./execute-all.sh ./scripts/download-nexus-image.sh ./scripts/extract-factory-images.sh ./scripts/generate-vendor.sh ./scripts/gen-prop-blobs-list.sh ./scripts/realpath.sh ./scripts/system-img-repair.sh; do
-        sed -i '2 i export PATH=$PATH:${stdenv.lib.makeBinPath [ zip unzip simg2img dexrepair e2fsprogs jq jdk wget utillinux perl which ]}' $i
+        sed -i '2 i export PATH=$PATH:${stdenv.lib.makeBinPath [ zip unzip simg2img dexrepair e2fsprogs jq jdk utillinux perl curl ]}' $i
     done
   '';
 
@@ -76,7 +68,7 @@ in
 
   configurePhase = ":";
 
-  postFixup = lib.optionalString (api == "29") ''
+  postFixup = ''
     wrapProgram $out/scripts/extract_android_ota_payload/extract_android_ota_payload.py \
       --prefix PYTHONPATH : "$PYTHONPATH"
   '';
