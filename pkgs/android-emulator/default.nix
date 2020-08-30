fc4af618733bc46da4285f4e4dd6dc9af88bff78
Daniel Fullmer <danielrf12@gmail.com>
2020-08-29 18:05:13 -0700

    emulator: use emulator from androidPkgs

--- pkgs/android-emulator/default.nix
+++ pkgs/android-emulator/default.nix
@@ -1,7 +1,8 @@
 { stdenv, lib, fetchurl, runCommand, writeText, writeShellScript,
   autoPatchelfHook, makeWrapper, glibc, libGL, libpulseaudio, zlib, ncurses5,
   nspr, fontconfig, nss, unzip, alsaLib, libuuid, xlibs, dbus, xkeyboard_config,
-  xorg
+  xorg,
+  androidPkgs
 }:
 
 let
@@ -58,61 +59,32 @@ let
         (path: value: (builtins.concatStringsSep "." path) + "=" + (builtins.toString value))
         conf));
 
-  android-emulator = stdenv.mkDerivation rec {
-    pname = "android-emulator";
-    version = "3.0.5";
+  bindImg = img: let
+    fakeSdkRoot = runCommand "fake-sdk" {} ''
+      mkdir -p $out/system-images/android
+      ln -s ${img} $out/system-images/android/x86
 
-    # https://androidstudio.googleblog.com/2020/03/emulator-3005-canary.html
-    # From https://dl.google.com/android/repository/repository2-1.xml
-    src = fetchurl {
-      url = "https://dl.google.com/android/repository/emulator-linux-6306047.zip";
-      sha256 = "0crgcawp366mql9zhr5xp4vlfklijrnp9ii706cssmlc3sz5jspz";
-    };
-
-    buildInputs = [
-      unzip autoPatchelfHook makeWrapper
-      glibc stdenv.cc.cc nss libGL libpulseaudio zlib ncurses5 nspr fontconfig
-      alsaLib libuuid
-    ] ++ (with xlibs; [
-      libX11 libXext libXdamage libXfixes libXcomposite libXcursor libXi
-      libXrender libXtst libxcb
-    ]);
-
-    # Some of this is from nixpkgs android sdk emulator derivation
-    postFixup = ''
-      # Wrap emulator so that it can load libdbus-1.so at runtime and it no longer complains about XKB keymaps
-      wrapProgram $out/emulator \
-        --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ dbus ]} \
-        --set QT_XKB_CONFIG_ROOT ${xkeyboard_config}/share/X11/xkb \
-        --set QTCOMPOSE ${xorg.libX11.out}/share/X11/locale
+      mkdir -p $out/platforms
+      mkdir -p $out/platform-tools
     '';
-    installPhase = ''
-      cp -r --reflink=auto . $out/
-    '';
+  in writeShellScript "bound-android-emulator" ''
+    AVD=$(pwd)/avd
+    mkdir -p $AVD/Pixel2.avd
+    cp ${writeText "config.ini" (toAVD defaultAVD)}    $AVD/Pixel2.avd/config.ini
+    echo "avd.ini.encoding=UTF-8"      > $AVD/Pixel2.ini
+    echo "target=android-29"          >> $AVD/Pixel2.ini
+    echo "path=$AVD/Pixel2.avd"       >> $AVD/Pixel2.ini
+    chmod u+w $AVD/Pixel2.ini $AVD/Pixel2.avd/config.ini
 
-    passthru = {
-      bindImg = img: let
-        fakeSdkRoot = runCommand "fake-sdk" {} ''
-          mkdir -p $out/system-images/android
-          ln -s ${img} $out/system-images/android/x86
+    export ANDROID_SDK_ROOT=${fakeSdkRoot}
+    export ANDROID_AVD_HOME=$AVD
 
-          mkdir -p $out/platforms
-          mkdir -p $out/platform-tools
-        '';
-      in writeShellScript "bound-android-emulator" ''
-        AVD=$(pwd)/avd
-        mkdir -p $AVD/Pixel2.avd
-        cp ${writeText "config.ini" (toAVD defaultAVD)}    $AVD/Pixel2.avd/config.ini
-        echo "avd.ini.encoding=UTF-8"      > $AVD/Pixel2.ini
-        echo "target=android-29"          >> $AVD/Pixel2.ini
-        echo "path=$AVD/Pixel2.avd"       >> $AVD/Pixel2.ini
-        chmod u+w $AVD/Pixel2.ini $AVD/Pixel2.avd/config.ini
+    ${android-emulator}/emulator @Pixel2 -gpu swiftshader_indirect $@
+  '';
 
-        export ANDROID_SDK_ROOT=${fakeSdkRoot}
-        export ANDROID_AVD_HOME=$AVD
-
-        ${android-emulator}/emulator @Pixel2 -gpu swiftshader_indirect $@
-      '';
+  android-emulator = androidPkgs.packages.canary.emulator.overrideAttrs ({ passthru ? {}, ...}: {
+    passthru = passthru // {
+      inherit bindImg;
     };
-  };
+  });
 in android-emulator
