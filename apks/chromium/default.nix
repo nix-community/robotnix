ebf88d71581ce6dec1af0868a1b8f91097774069
Daniel Fullmer <danielrf12@gmail.com>
2020-09-08 21:13:35 -0700

    chromium: 84.0.4147.111 -> 85.0.4183.101

--- apks/chromium/default.nix
+++ apks/chromium/default.nix
@@ -13,7 +13,7 @@
 , buildTargets ? [ "chrome_modern_public_apk" ]
 , packageName ? "org.chromium.chrome"
 , webviewPackageName ? "com.android.webview"
-, version ? "84.0.4147.111"
+, version ? "85.0.4183.101"
 , versionCode ? null
 # Potential buildTargets:
 # chrome_modern_public_apk + system_webview_apk
