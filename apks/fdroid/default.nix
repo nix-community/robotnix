c09245ea9a375ed5b580340e806cdc181f4f9808
Daniel Fullmer <danielrf12@gmail.com>
2020-10-24 11:52:16 -0700

    fdroid: 1.9 -> 1.10

--- apks/fdroid/default.nix
+++ apks/fdroid/default.nix
@@ -1,11 +1,11 @@
 { callPackage, substituteAll, fetchFromGitLab, androidPkgs, jdk, gradle }:
 let
-  androidsdk = androidPkgs.sdk (p: with p.stable; [ tools platforms.android-27 build-tools-27-0-3 ]);
+  androidsdk = androidPkgs.sdk (p: with p.stable; [ tools platforms.android-28 build-tools-28-0-3 ]);
   buildGradle = callPackage ./gradle-env.nix {};
 in
 buildGradle rec {
   name = "F-Droid-${version}.apk";
-  version = "1.9";
+  version = "1.10";
 
   envSpec = ./gradle-env.json;
 
@@ -13,7 +13,7 @@ buildGradle rec {
     owner = "fdroid";
     repo = "fdroidclient";
     rev = version;
-    sha256 = "1ijwl8q4530spz87jjvfi83rjxkpz0srly5ijbj27v4xbf35pkm6";
+    sha256 = "01kkpgzsnc2h3w0k1q44pdgqybiwsl5wwwabq0df5my6kw661c2y";
   };
 
   patches = [
@@ -24,10 +24,11 @@ buildGradle rec {
     substituteInPlace app/build.gradle --replace "getVersionName()" "\"${version}\""
   '';
 
-  gradleFlags = [ "assembleRelease" ];
+  # Lenient dependency verification needed so we can patch aapt2. It's hash is verifid by nix anyway
+  gradleFlags = [ "-Dorg.gradle.dependency.verification=lenient" "assembleRelease" ];
 
   ANDROID_HOME = "${androidsdk}/share/android-sdk";
-  nativeBuildInputs = [ jdk gradle ];
+  nativeBuildInputs = [ jdk ];
 
   installPhase = ''
     cp app/build/outputs/apk/full/release/app-full-release-unsigned.apk $out
