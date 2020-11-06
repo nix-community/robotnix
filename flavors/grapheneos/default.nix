28b8275724c131ce335930fad96321651d42ff08
Daniel Fullmer <danielrf12@gmail.com>
2020-11-05 20:58:50 -0800

    grapheneos: RP1A.201105.002.2020.11.03.03 -> RP1A.201105.002.2020.11.05.18

--- flavors/grapheneos/default.nix
+++ flavors/grapheneos/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 with lib;
 let
-  grapheneOSRelease = "${config.apv.buildID}.2020.11.03.03";
+  grapheneOSRelease = "${config.apv.buildID}.2020.11.05.18";
 
   phoneDeviceFamilies = [ "crosshatch" "bonito" "coral" "sunfish" ];
   supportedDeviceFamilies = phoneDeviceFamilies ++ [ "generic" ];
@@ -11,7 +11,7 @@ in mkIf (config.flavor == "grapheneos") (mkMerge [
   # This a default datetime for robotnix that I update manually whenever
   # significant a change is made to anything the build depends on. It does not
   # match the datetime used in the GrapheneOS build above.
-  buildDateTime = mkDefault 1604435512;
+  buildDateTime = mkDefault 1604638695;
 
   source.dirs = lib.importJSON (./. + "/repo-${grapheneOSRelease}.json");
 
