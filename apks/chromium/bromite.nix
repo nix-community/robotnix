008b3c914981b524c3292bcfeb085ecb41628db3
Daniel Fullmer <danielrf12@gmail.com>
2020-12-12 22:10:38 -0800

    bromite: 87.0.4280.68 -> 87.0.4280.106

--- apks/chromium/bromite.nix
+++ apks/chromium/bromite.nix
@@ -1,13 +1,13 @@
 { chromium, fetchFromGitHub, git, python3 }:
 
 let
-  version = "87.0.4280.68";
+  version = "87.0.4280.106";
 
   bromite_src = fetchFromGitHub {
     owner = "bromite";
     repo = "bromite";
     rev = version;
-    sha256 = "0c5h63g8j71d5msdwmvf15j29h9n738hg48xl20jljjladckdy9y";
+    sha256 = "16cgaikpqwh47f0y1gds4zllias2nhh9qgfnz3r6z02yzq4sb3hg";
   };
 
 in (chromium.override {
