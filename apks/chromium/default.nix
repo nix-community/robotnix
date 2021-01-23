afd2a3319521f3bf5c14aaa3f983f46946f91795
Daniel Fullmer <danielrf12@gmail.com>
2021-01-22 21:32:39 -0800

    chromium: 87.0.4280.141 -> 88.0.4324.93
    The sub-repo at
    https://chromium.googlesource.com/external/github.com/freetype/freetype2-testing
    for the specific revision in this tag had a git submodule that was
    missing an entry in .gitmodules, so it would did not check out properly.
    I've manually set the revision for this sub-repo to a commit which did
    work.

--- apks/chromium/default.nix
+++ apks/chromium/default.nix
@@ -14,7 +14,7 @@
 , packageName ? "org.chromium.chrome"
 , webviewPackageName ? "com.android.webview"
 , trichromeLibraryPackageName ? "org.chromium.trichromelibrary"
-, version ? "87.0.4280.141"
+, version ? "88.0.4324.93"
 , versionCode ? null
 # Potential buildTargets:
 # chrome_modern_public_bundle + system_webview_apk
