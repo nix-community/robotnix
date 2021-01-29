38ee3aff3e08e3d4d3b4b7b1272a0b59b748e027
Daniel Fullmer <danielrf12@gmail.com>
2021-01-29 14:53:47 -0800

    apv: improve wording in apv-lib-check.sh

--- modules/apv-lib-check.sh
+++ modules/apv-lib-check.sh
@@ -14,7 +14,7 @@ check_partition() {
 
         while read -r required_lib; do
             if [[ ! (-f "$BUILT/$partition/$libdir/$required_lib" || -L "$BUILT/$partition/$libdir/$required_lib" ) && -e "$UPSTREAM/$partition/$libdir/$required_lib" ]]; then
-                echo "$partition/$libdir/$required_lib needed by $lib"
+                echo "$partition/$libdir/$required_lib is needed by $lib"
             fi
         done < <(readelf -d "$lib" | grep NEEDED | tr -s ' ' | cut -d ' ' -f6 | tr -d '[]')
     done < <(find "$partition" -type f -name '*.so')
