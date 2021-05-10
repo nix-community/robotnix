5ace901433d25b8abb143f0b70f145574091a946
Daniel Fullmer <danielrf12@gmail.com>
2021-05-10 10:16:41 -0700

    diffoscope: fix issue with incorrect apkFileName
    Upstream issue:
    https://salsa.debian.org/reproducible-builds/diffoscope/-/issues/255

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -8,6 +8,12 @@ self: super: {
   diffoscope = (super.diffoscope.overrideAttrs (attrs: rec {
     patches = attrs.patches ++ [
       ./diffoscope/0001-comparators-android-Support-sparse-android-images.patch
+
+      # Fix for https://salsa.debian.org/reproducible-builds/diffoscope/-/issues/255
+      (super.fetchpatch {
+        url = "https://salsa.debian.org/reproducible-builds/diffoscope/-/commit/6bf636c9f4e9f7d5b300723855bb2ac6cb9b11a1.patch";
+        sha256 = "04m4f7z1lfbn2c3pkyhrb1ibafdllmqvf25swy31d19hi5ywgjy8";
+      })
     ];
     pythonPath = attrs.pythonPath ++ [ super.simg2img super.zip ];
     doCheck = false;
