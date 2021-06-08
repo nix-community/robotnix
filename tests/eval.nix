8d3efc971543ff463e797f87dbf66149e7ff215c
Daniel Fullmer <danielrf12@gmail.com>
2021-06-08 12:48:22 -0700

    tests: add basic evaluation test

---
+++ tests/eval.nix
@@ -0,0 +1,32 @@
+# To run these tests:
+# nix-instantiate --eval --strict ./eval.nix
+# if the resulting list is empty, all tests passed
+
+let
+  pkgs = import ../pkgs {};
+  lib = pkgs.lib;
+  robotnixSystem = configuration: import ../default.nix { inherit configuration pkgs; };
+in
+
+lib.runTests {
+  testSourceMountPoints = {
+    expr = let
+      dirs = [ "a" "a/b" "a/c" "b/d" "b/e" ];
+    in
+      lib.filterAttrs (n: v: lib.elem n dirs)
+        ((lib.mapAttrs (name: dir: dir.postPatch))
+          (robotnixSystem {
+            source.dirs = lib.genAttrs dirs (dir: {});
+          }).config.source.dirs);
+    expected = {
+      "a" = ''
+        mkdir b
+        mkdir c
+      '';
+      "a/b" = "";
+      "a/c" = "";
+      "b/d" = "";
+      "b/e" = "";
+    };
+  };
+}
