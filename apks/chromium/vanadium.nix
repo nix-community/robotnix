527ded7997d8897add688a394d7c945b95215c25
Daniel Fullmer <danielrf12@gmail.com>
2021-07-20 09:35:29 -0700

    vanadium: 91.0.4472.134 -> 91.0.4472.164

--- apks/chromium/vanadium.nix
+++ apks/chromium/vanadium.nix
@@ -7,13 +7,13 @@ let
   vanadium_src = fetchFromGitHub {
     owner = "GrapheneOS";
     repo = "Vanadium";
-    rev = "RQ3A.210705.001.2021.07.07.19";
-    sha256 = "0a5lkydm7rzkvckh6sk1c62dr6vq3yfhkdwaj396qs1hb43ir5c1";
+    rev = "RQ3A.210705.001.2021.07.19.18";
+    sha256 = "15jq9s6jbf7ym3z8iml4z0d6xpx6c0by6vc4w31j0vs2qkplns18";
   };
 in (chromium.override {
   name = "vanadium";
   displayName = "Vanadium";
-  version = "91.0.4472.134";
+  version = "91.0.4472.164";
   enableRebranding = false; # Patches already include rebranding
   customGnFlags = {
     is_component_build = false;
