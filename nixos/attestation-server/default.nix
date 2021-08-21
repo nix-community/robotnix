6fce75eb619ce26caa794fba5003c9bd07abe9a8
Daniel Fullmer <danielrf12@gmail.com>
2021-08-21 14:28:48 -0700

    nixos/attestation-server: 2021-05-19 -> 2021-08-21

--- nixos/attestation-server/default.nix
+++ nixos/attestation-server/default.nix
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2020 Daniel Fullmer and robotnix contributors
 # SPDX-License-Identifier: MIT
 
-{ pkgs, callPackage, lib, substituteAll, makeWrapper, fetchFromGitHub, jdk11_headless, gradleGen,
+{ pkgs, callPackage, lib, substituteAll, makeWrapper, fetchFromGitHub, jdk16_headless, gradleGen,
   listenHost ? "localhost",
   port ? 8080,
   applicationId ? "org.robotnix.auditor",
@@ -13,22 +13,22 @@
 let
   buildGradle = callPackage ./gradle-env.nix {
     gradleGen = callPackage (pkgs.path + /pkgs/development/tools/build-managers/gradle) {
-      java = jdk11_headless;
+      java = jdk16_headless;
     };
   };
   supportedDevices = import ../../apks/auditor/supported-devices.nix;
 in
 buildGradle {
   pname = "AttestationServer";
-  version = "2021-05-19";
+  version = "2021-08-21";
 
   envSpec = ./gradle-env.json;
 
   src = fetchFromGitHub {
     owner = "grapheneos";
     repo = "AttestationServer";
-    rev = "d621e8a2c58dcac1f5cfc8231ca13b63cc911dcb";
-    sha256 = "1nrnhgmxdw4rf92fmsayi54l48ik893dnhjxcvn8y91b43y323kj";
+    rev = "769da1ef59dc57a160b160e548367de7094144e1";
+    sha256 = "1f8fj6rhg7bkkwzvqhsz1iwvzyj8l6d9r1n8p491dnwzvr7z93ka";
   };
 
   patches = [
@@ -38,6 +38,13 @@ buildGradle {
     }
     // lib.genAttrs supportedDevices (d: if (device == d) then avbFingerprint else "DISABLED_CUSTOM_${d}")))
   ];
+
+  postPatch = ''
+    for f in src/main/java/app/attestation/server/*.java  static/*.{html,txt}; do
+      substituteInPlace $f \
+        --replace "attestation.app" "${domain}"
+    done
+  '';
 
   JAVA_TOOL_OPTIONS = "-Dfile.encoding=UTF8";
 
@@ -50,7 +57,7 @@ buildGradle {
     mv build/libs/source.jar build/libs/AttestationServer.jar # "source" is just the name of the parent dir in the nix environment, which ought to be "AttestationServer"
     cp -r build/libs/* $out/share/java
 
-    makeWrapper ${jdk11_headless}/bin/java $out/bin/AttestationServer \
+    makeWrapper ${jdk16_headless}/bin/java $out/bin/AttestationServer \
       --add-flags "-cp $out/share/java/AttestationServer.jar:$out/share/java/* app.attestation.server.AttestationServer"
 
     # Static HTML output
