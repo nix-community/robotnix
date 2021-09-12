8d98328a2df69f07308496c3f43f42f734dd7d02
Daniel Fullmer <danielrf12@gmail.com>
2021-09-11 18:53:08 -0700

    Shellcheck all scripts under flavors/

--- scripts/check.sh
+++ scripts/check.sh
@@ -2,9 +2,11 @@
 
 set -euo pipefail
 
+shopt -s globstar
+
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 mypy --exclude apks/chromium .
 flake8 --exclude apks/chromium .
 pytest .
-shellcheck ./*.sh flavors/*/*.sh modules/pixel/update.sh
+shellcheck ./*.sh flavors/**/*.sh modules/pixel/update.sh
