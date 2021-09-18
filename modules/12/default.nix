77871c57f60189b218ee89296f6ffc5efddfdce1
Daniel Fullmer <danielrf12@gmail.com>
2021-09-18 15:19:33 -0700

    Remove mk_sources_zip.py patch for Android 12

--- modules/12/default.nix
+++ modules/12/default.nix
@@ -6,18 +6,10 @@
 let
   inherit (lib) mkIf;
 in
-mkIf (config.androidVersion == 12) (mkMerge [
-{
+mkIf (config.androidVersion == 12) {
   source.dirs."build/make".patches = [
     ./build_make/0001-Readonly-source-fix.patch
   ];
 
-  # This one script needs python2. Used by sdk builds
-  source.dirs."development".postPatch = ''
-    substituteInPlace build/tools/mk_sources_zip.py \
-      --replace "#!/usr/bin/python" "#!${pkgs.python2.interpreter}"
-  '';
-
   #kernel.clangVersion = mkDefault "r370808";
 }
-])
