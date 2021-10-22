f542b8e84861be0bf04eff8764a35c7bcc8d77a2
Daniel Fullmer <danielrf12@gmail.com>
2021-10-21 19:58:14 -0700

    apv: add support for Android 12 w/ vanilla flavor

--- pkgs/android-prepare-vendor/default.nix
+++ pkgs/android-prepare-vendor/default.nix
@@ -12,43 +12,34 @@ let
   dexrepair = callPackage ./dexrepair.nix {};
   apiStr = builtins.toString api;
 
-  # TODO: Build this ourselves?
-  oatdump = stdenv.mkDerivation {
-    name = "oatdump-${apiStr}";
+  version = {
+    "29" = "2020-08-26";
+    "30" = "2021-09-07";
+    "31" = "2021-10-21";
+  }.${builtins.toString api};
 
-    src = fetchurl {
-      url = https://onedrive.live.com/download?cid=D1FAC8CC6BE2C2B0&resid=D1FAC8CC6BE2C2B0%21574&authkey=ADSQA_DtfAmmk2c;
-      name = "oatdump-${apiStr}.zip";
-      sha256 = "0kiq173jqg6qzw9m5wwp0kh1d3zxxksi69xj4nwg7pp43m4lfjir";
-    };
-
-    nativeBuildInputs = [ autoPatchelfHook ];
-
-    unpackPhase = ''
-      ${unzip}/bin/unzip $src
-    '';
-
-    installPhase = ''
-      mkdir -p $out
-      cp -r * $out
-    '';
-  };
-
-  version = if api >= 30 then "2021-09-07" else "2020-08-26";
-  src = if api >= 30
-    then fetchFromGitHub {
+  src = {
+    "30" = (fetchFromGitHub {
       # Android11 branch
       owner = "AOSPAlliance";
       repo = "android-prepare-vendor";
       rev = "227f5ce7cd89a3f57291fe2b84869c7a5d1e17fa";
       sha256 = "07g5dcl2x44ai5q2yfq9ybx7j7kn41s82hgpv7jff5v1vr38cia9";
-    } else fetchFromGitHub {
+    });
+    "29" = (fetchFromGitHub {
       # Android10 branch
       owner = "AOSPAlliance";
       repo = "android-prepare-vendor";
       rev = "a9602ca6ef16ff10641d668dcb203f89f402d40d";
       sha256 = "0wldj8ykwh8r7m1ff6vbkbc73a80lmmxwfmk8nm0cnzpbfk4cq7w";
-    };
+    });
+    "31" = (fetchFromGitHub {
+      owner = "grapheneos";
+      repo = "android-prepare-vendor";
+      rev = "7c09cb887d3b9a2643cfc6ecf3966db1e378be32";
+      sha256 = "0j579ick4cihv3ha2gg0b88h33zfik118376g4rw1qfq0cwbwdg8";
+    });
+  }.${builtins.toString api};
 
 in
 (stdenv.mkDerivation {
@@ -60,21 +51,21 @@ in
     (python.withPackages (p: [ p.protobuf ])) # Python is used by "extract_android_ota_payload"
   ];
 
-  patches = [
-    ./0001-Disable-oatdump-update.patch
-    ./0002-Just-write-proprietary-blobs.txt-to-current-dir.patch
-    ./0003-Allow-for-externally-set-config-file.patch
-    ./0004-Add-option-to-use-externally-provided-carrier_list.p.patch
+  patches = if api <= 30 then [
+    ./11/0001-Disable-oatdump-update.patch
+    ./11/0002-Just-write-proprietary-blobs.txt-to-current-dir.patch
+    ./11/0003-Allow-for-externally-set-config-file.patch
+    ./11/0004-Add-option-to-use-externally-provided-carrier_list.p.patch
+  ] else [
+    ./12/0001-Just-write-proprietary-blobs.txt-to-current-dir.patch
+    ./12/0002-Allow-for-externally-set-config-file.patch
+    ./12/0003-Add-option-to-use-externally-provided-carrier_list.p.patch
+    ./12/0004-Add-Android-12-workaround-for-PRODUCT_COPY_FILES.patch
   ];
 
-  # TODO: No need to copy oatdump now that we're making a standalone android-prepare-vendor.
-  # Just patch it out instead
   postPatch = ''
     patchShebangs ./execute-all.sh
     patchShebangs ./scripts
-    # TODO: Hardcoded api version
-    mkdir -p hostTools/Linux/api-${apiStr}/
-    cp -r ${oatdump}/* hostTools/Linux/api-${apiStr}/
 
     for i in ./execute-all.sh ./scripts/download-nexus-image.sh ./scripts/extract-factory-images.sh ./scripts/generate-vendor.sh ./scripts/gen-prop-blobs-list.sh ./scripts/realpath.sh ./scripts/system-img-repair.sh ./scripts/extract-ota.sh; do
         sed -i '2 i export PATH=$PATH:${lib.makeBinPath [ zip unzip simg2img dexrepair e2fsprogs jq jdk utillinux perl curl libarchive ]}' $i
