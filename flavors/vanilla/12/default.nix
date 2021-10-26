84f3b1e2291eddf8b2ad1526aa2b3e095114098a
Daniel Fullmer <danielrf12@gmail.com>
2021-10-25 21:47:55 -0700

    vanilla: switch power button long press to show global actions menu

--- flavors/vanilla/12/default.nix
+++ flavors/vanilla/12/default.nix
@@ -36,6 +36,9 @@ in
     type = "string-array";
   };
 
+  # Override default power button long press behavior to bring up "global actions menu" instead of the (missing in AOSP) assistant.
+  resources."frameworks/base/core/res".config_longPressOnPowerBehavior = 1;
+
   # Temporary fix for crashes
   # https://github.com/GrapheneOS/platform_frameworks_base/commit/3c81f90076fbb49efb0bdd86826695ab88cd085f
   resources."frameworks/base/core/res".config_appsNotReportingCrashes = "com.android.statementservice";
