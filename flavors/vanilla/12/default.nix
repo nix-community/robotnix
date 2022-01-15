230dcbcf982f0fc97658378a520af2451418e65f
Zhaofeng Li <hello@zhaofeng.li>
2022-01-15 15:33:21 -0800

    vanilla/raviole: SQ1D.211205.017 -> SQ1D.220105.007

--- flavors/vanilla/12/default.nix
+++ flavors/vanilla/12/default.nix
@@ -15,11 +15,11 @@ in
   buildDateTime = mkDefault 1641669748;
 
   source.manifest.rev = mkDefault (
-    if (config.deviceFamily == "raviole") then "android-12.0.0_r19"
+    if (config.deviceFamily == "raviole") then "android-12.0.0_r27"
     else "android-12.0.0_r26"
   );
   apv.buildID = mkDefault (
-    if (config.deviceFamily == "raviole") then "SQ1D.211205.017"
+    if (config.deviceFamily == "raviole") then "SQ1D.220105.007"
     else "SQ1A.220105.002"
   );
 
