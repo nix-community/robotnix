ee5cebf3cf9829e9a77c02a25455150a0c8520f4
Daniel Fullmer <danielrf12@gmail.com>
2022-01-22 17:05:55 -0800

    release.nix: remove recurseIntoAttrs
    nix-eval-jobs doesn't respect these

--- release.nix
+++ release.nix
@@ -43,20 +43,20 @@ in
 {
   inherit (pkgs) diffoscope;
 
-  imgs = lib.recurseIntoAttrs (lib.mapAttrs (name: c: c.img) builtConfigs);
+  imgs = lib.mapAttrs (name: c: c.img) builtConfigs;
 
   # Generates img and ota files for each configuration using snakeoil keys
-  signingCheck = lib.recurseIntoAttrs (lib.mapAttrs (name: c: lib.recurseIntoAttrs { inherit (robotnix { imports = [ snakeoilSignedModule c ]; }) img ota; }) {
+  signingCheck = lib.mapAttrs (name: c:  { inherit (robotnix { imports = [ snakeoilSignedModule c ]; }) img ota; }) {
     "lineageos-10" = { device="marlin"; flavor="lineageos"; androidVersion=10; };
     "vanilla-10" = { device="sunfish"; flavor="vanilla"; androidVersion=10; apv.enable=false; pixel.useUpstreamDriverBinaries=true; }; # APV not working on Android 10...
     "vanilla-11" = { device="sunfish"; flavor="vanilla"; androidVersion=11; };
     "vanilla-12" = { device="sunfish"; flavor="vanilla"; androidVersion=12; };
-  });
+  };
 
   # For testing instantiation
-  vanilla-arm64 = lib.recurseIntoAttrs {
+  vanilla-arm64 = {
     inherit (defaultBuild)
-      targetFiles ota img factoryImg bootImg otaDir
+      ota img factoryImg bootImg otaDir
       releaseScript;
   };
 
@@ -67,11 +67,11 @@ in
   vanilla-12-emulator = (robotnix { device="x86_64"; flavor="vanilla"; productNamePrefix="sdk_phone_"; androidVersion=12; }).emulator;
   danielfullmer-emulator = (robotnix { device="x86_64"; flavor="grapheneos"; imports = [ ./example.nix ]; apps.auditor.enable = lib.mkForce false; }).emulator;
 
-  tests = lib.recurseIntoAttrs tests;
+  inherit tests;
 
   # Stuff to upload to binary cache
-  cached = lib.recurseIntoAttrs {
-    browsers = lib.recurseIntoAttrs {
+  cached = {
+    browsers = {
       inherit ((robotnix { device = "arm64"; flavor="vanilla"; }).config.build)
         chromium;
       inherit ((robotnix { device = "arm64"; flavor="vanilla"; apps.bromite.enable=true; webview.bromite.enable=true; }).config.build)
@@ -80,16 +80,16 @@ in
         vanadium;
     };
 
-    kernels = lib.recurseIntoAttrs
+    kernels =
       (lib.mapAttrs (name: c: c.config.build.kernel)
         (lib.filterAttrs (name: c: c.config.kernel.enable) builtConfigs));
 
-    tests = lib.recurseIntoAttrs {
+    tests = {
       attestation-server = tests.attestation-server.test;
       inherit (tests) generateKeys;
     };
 
-    packages = lib.recurseIntoAttrs {
+    packages = {
       inherit (pkgs) cipd;
     };
   };
