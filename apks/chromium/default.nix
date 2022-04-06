990295260f7d6cf06f0d3d27d7b860e46ce204fb
Daniel Fullmer <danielrf12@gmail.com>
2022-04-05 18:29:47 -0700

    chromium: 100.0.4896.58 -> 100.0.4896.79

--- apks/chromium/default.nix
+++ apks/chromium/default.nix
@@ -18,7 +18,7 @@
 , packageName ? "org.chromium.chrome"
 , webviewPackageName ? "com.android.webview"
 , trichromeLibraryPackageName ? "org.chromium.trichromelibrary"
-, version ? "100.0.4896.58"
+, version ? "100.0.4896.79"
 , versionCode ? null
 # Potential buildTargets:
 # chrome_modern_public_bundle + system_webview_apk
