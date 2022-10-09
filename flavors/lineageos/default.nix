c783591495af320518a14fc696f29c5a25b1d833
Atemu <atemu.main@gmail.com>
2022-10-09 17:55:10 +0200

    lineageos-19.1: 2022052416 -> 2022100915

--- flavors/lineageos/default.nix
+++ flavors/lineageos/default.nix
@@ -75,7 +75,7 @@ in mkIf (config.flavor == "lineageos")
 
   productNamePrefix = "lineage_"; # product names start with "lineage_"
 
-  buildDateTime = mkDefault 1653409034;
+  buildDateTime = mkDefault 1665330168;
 
   # LineageOS uses this by default. If your device supports it, I recommend using variant = "user"
   variant = mkDefault "userdebug";
