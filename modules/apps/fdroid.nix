13da61c060b2d7fb8e0b3e3aee35c46e128d2f88
eyjhb <eyjhbb@gmail.com>
2024-06-03 10:12:53 +0200

    fdroid: prebuilt-apk: 1.17.0 -> 1.20.0

--- modules/apps/fdroid.nix
+++ modules/apps/fdroid.nix
@@ -76,8 +76,8 @@ in
   config = mkIf cfg.enable {
     apps.prebuilt."F-Droid" = {
       apk = pkgs.fetchurl {
-        url = "https://f-droid.org/repo/org.fdroid.fdroid_1017050.apk";
-        sha256 = "sha256-3Du4j2QZ7n3efRVHpBVpqgMoL+AODcQ84DXv18nSfXU=";
+        url = "https://f-droid.org/repo/org.fdroid.fdroid_1020050.apk";
+        sha256 = "sha256-wWd/so9s/Ahdp6WtFbp1sjfNlbt4pnmqqNsMGt1o8QQ=";
       };
 
       fingerprint = mkIf (!config.signing.enable) "7352DAE94B237866E7FB44FD94ADE44E8B6E05397E7D1FB45616A00E225063FF";
