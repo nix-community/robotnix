67048aecfc906b5f2206ff14a5b1e0abc2cebcf7
Atemu <git@atemu.net>
2025-01-28 19:02:39 +0100

    fdroid: 1021051 -> 1022050

--- modules/apps/fdroid.nix
+++ modules/apps/fdroid.nix
@@ -78,13 +78,13 @@ in
       apk = pkgs.fetchurl {
         urls =
           let
-            version = "1021051";
+            version = "1022050";
           in
           [
             "https://f-droid.org/repo/org.fdroid.fdroid_${version}.apk"
             "https://f-droid.org/archive/org.fdroid.fdroid_${version}.apk"
           ];
-        sha256 = "sha256-tkZRhJ7OtXRZBJ5wF+BDAI/OjxRqrR002Y8AJ9h+Tu8=";
+        sha256 = "sha256-+tnudVUCpeOHhaW9E/x9cPYHGFF4tZ/jOQQNOCPs19k=";
       };
 
       fingerprint = mkIf (!config.signing.enable) "7352DAE94B237866E7FB44FD94ADE44E8B6E05397E7D1FB45616A00E225063FF";
