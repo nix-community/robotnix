3885a26ccc9904140569c1376c314ee5455dca0b
cyclopentane <cyclopentane@aidoskyneen.eu>
2025-06-24 17:03:03 +0200

    NixOS 25.05 and flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'androidPkgs':
        'github:tadfisher/android-nixpkgs/4f5fcd7540c6927cd643594b34c878151507f283' (2025-01-23)
      → 'github:tadfisher/android-nixpkgs/0846fab1f060f646e1017053077ad38dedc5207b' (2025-06-23)
    • Updated input 'androidPkgs/devshell':
        'github:numtide/devshell/f7795ede5b02664b57035b3b757876703e2c3eac' (2024-12-31)
      → 'github:numtide/devshell/7c9e793ebe66bcba8292989a68c0419b737a22a0' (2025-03-08)
    • Updated input 'androidPkgs/nixpkgs':
        'github:NixOS/nixpkgs/9e4d5190a9482a1fb9d18adf0bdb83c6e506eaab' (2025-01-21)
      → 'github:NixOS/nixpkgs/4206c4cb56751df534751b058295ea61357bbbaa' (2025-06-21)
    • Updated input 'flake-compat':
        'github:nix-community/flake-compat/8bf105319d44f6b9f0d764efa4fdef9f1cc9ba1c' (2023-06-29)
      → 'github:nix-community/flake-compat/0f158086a2ecdbb138cd0429410e44994f1b7e4b' (2025-05-02)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/6a3ae7a5a12fb8cac2d59d7df7cbd95f9b2f0566' (2025-01-18)
      → 'github:NixOS/nixpkgs/c7ab75210cb8cb16ddd8f290755d9558edde7ee1' (2025-06-22)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Build Android (AOSP) using Nix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
     androidPkgs.url = "github:tadfisher/android-nixpkgs/stable";
 
