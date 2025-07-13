b415756399f01c4194863828d41d7d220595c86d
cyclopentane <cyclopentane@aidoskyneen.eu>
2025-07-13 11:07:21 +0200

    Add update script for GrapheneOS

---
+++ flavors/grapheneos/update.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+repo-tool get-graphene-devices -c stable -c beta -c alpha devices.json channel-info.json
+tags=$(jq -r .git_tags[] channel-info.json | sort -r)
+latest_tag=$(jq -r .git_tags[-1] channel-info.json)
+
+for tag in $tags; do
+  if [ ! -e $tag.lock ]; then
+    echo No lockfile for $tag yet.
+    if [ -e $latest_tag.lock ]; then
+      echo Copying from latest tag $latest_tag.
+      cp $latest_tag.lock $tag.lock
+    fi
+  fi
+  repo-tool fetch --tag -b $tag https://github.com/GrapheneOS/platform_manifest $tag.lock
+  lockfiles="$lockfiles $tag.lock"
+done
+
+
+repo-tool get-build-id build_ids.json $lockfiles
