8153c0d95c607662f91942d296e9c84c2481d198
cyclopentane <cyclopentane@aidoskyneen.eu>
2025-07-16 01:08:19 +0200

    Fix cipd derivation

--- pkgs/cipd/default.nix
+++ pkgs/cipd/default.nix
@@ -1,14 +1,13 @@
 # SPDX-FileCopyrightText: 2020 Daniel Fullmer and robotnix contributors
 # SPDX-License-Identifier: MIT
 
-{ lib, stdenv, buildGoPackage, fetchgit, fetchhg, fetchbzr, fetchsvn }:
+{ lib, stdenv, buildGoModule, fetchgit, fetchhg, fetchbzr, fetchsvn }:
 
-buildGoPackage rec {
+buildGoModule rec {
   name = "cipd-${version}";
   version = "2019-12-13";
   rev = "95480de32ef149429a7a6ab0e5c7380bfb0f102b";
 
-  goPackagePath = "go.chromium.org/luci";
   subPackages = [ "cipd/client/cmd/cipd" ];
 
   src = fetchgit {
@@ -17,7 +16,7 @@ buildGoPackage rec {
     sha256 = "0hkg2j4y7vqjhfvlgkfjpc0hcrrb08f6nmz00zxrsf7735lv09i9";
   };
 
-  goDeps = ./deps.nix;
+  vendorHash = null;
 
   meta = with lib; {
     description = "Chrome Infrastructure Package Deployment";
