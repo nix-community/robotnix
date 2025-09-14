832995c3395a0634ae10f0efbcdfbb076ebb9989
Mio <mio@mio19.uk>
2025-09-14 12:37:21 +1000

    ./0001-Revert-mkuserimg_mke2fs.py-Package-mke2fs.conf.patch for lineageos 23.0

--- flavors/lineageos/default.nix
+++ flavors/lineageos/default.nix
@@ -83,11 +83,13 @@ in mkIf (config.flavor == "lineageos") {
     ];
     "system/extras".patches = [
       # pkgutil.get_data() not working, probably because we don't use their compiled python
-      (pkgs.fetchpatch {
+      (if lib.versionAtLeast config.flavorVersion "23.0"
+      then ./0001-Revert-mkuserimg_mke2fs.py-Package-mke2fs.conf.patch
+      else (pkgs.fetchpatch {
         url = "https://github.com/LineageOS/android_system_extras/commit/7da4b29321eb7ebce9eb9a43d0fbd85d0aa1e870.patch";
         sha256 = "0pv56lypdpsn66s7ffcps5ykyfx0hjkazml89flj7p1px12zjhy1";
         revert = true;
-      })
+      }))
     ];
 
     # LineageOS will sometimes force-push to this repo, and the older revisions are garbage collected.
