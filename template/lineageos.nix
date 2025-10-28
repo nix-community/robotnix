3ecab55be90514933b10d3a531cc4d3086c90091
cyclopentane <cyclopentane@aidoskyneen.eu>
2025-10-28 20:03:24 +0100

    Bump branch in LineageOS template to 23.0

--- template/lineageos.nix
+++ template/lineageos.nix
@@ -9,7 +9,7 @@
   # You can check the supported branches for your device under
   # https://wiki.lineageos.org/devices/<device codename>
   # Leave out to choose the official default branch for the device.
-  flavorVersion = "22.2";
+  flavorVersion = "23.0";
 
   apps.fdroid.enable = true;
   microg.enable = true;
