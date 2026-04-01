4786eb57ed8bedde72b19d9cf970e569b258cf10
cyclic-pentane <cyclic-pentane@users.noreply.github.com>
2026-04-01 17:25:14 +0000

    deploy: f7ed3f5f4b44ab247f422863fc970ac3172ae952

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-543a464b.js',
+                path_to_root + 'searchindex-aeb4db28.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
